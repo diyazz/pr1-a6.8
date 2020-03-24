ca4b2f6a159835efbc85c1b73ff7e52db89ed7bf
diyazz <61910498+diyazz@users.noreply.github.com>
2020-03-24 23:03:55 +0600

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,19 @@
+
+
+var pr_br =  0;
+
+function b1() {
+	pr_br += 1;
+	$("#my-progress-bar").width(pr_br+"%");
+}
+
+function b2() {
+	pr_br += 3;
+	$("#my-progress-bar").width(pr_br+"%");
+}
+
+function b3() {
+	pr_br += 7;
+	$("#my-progress-bar").width(pr_br+"%");
+}
+
